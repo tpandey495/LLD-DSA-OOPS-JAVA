c395973ae6bd7ab49e33c3e2f0e5470c55799b68
Tarun Pandey <tpandey495@gmail.com>
2024-12-24 22:26:04 +0530

    folder

--- java-pratice/src/Main.java
+++ java-pratice/src/Main.java
@@ -1,10 +1,12 @@
+import java.util.Calendar;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
 
     public static void main(String[] args) {
-          calender
+          new Calendar()
 
         }
     }
--- out/production/data-structure-implimentation/LLD/Factory/Readme.md
+++
@@ -1,7 +0,0 @@
-## Factory Design Patter
-```
-Factory Method is a creational design pattern 
-that provides an interface for creating objects 
-in a superclass,but allows subclass to alter the type of objects
-that will be created.
-```
